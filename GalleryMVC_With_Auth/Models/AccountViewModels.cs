aa07db5b1dbf89d02135051c736c7b4bfd87b7d9
Anton Burkin <ramzzzay@gmail.com>
2015-08-13 00:17:34 +0300

    Add: prop Name to AccountViewModels

--- GalleryMVC_With_Auth/Models/AccountViewModels.cs
+++ GalleryMVC_With_Auth/Models/AccountViewModels.cs
@@ -79,6 +79,8 @@ namespace GalleryMVC_With_Auth.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
