b604e2c077cbc9e0870e7944407814bce8c19777
Anton Burkin <ramzzzay@gmail.com>
2015-08-12 15:54:06 +0300

    Add: ErrorsController

---
+++ GalleryMVC_With_Auth/Controllers/ErrorsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GalleryMVC_With_Auth.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        // GET: Errors
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
