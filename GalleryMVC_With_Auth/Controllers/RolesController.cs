ce280e0690f9b3376168d9dc0172e6066ec460ab
Anton Burkin <ramzzzay@gmail.com>
2015-08-13 00:11:40 +0300

    add: Controller Roles

---
+++ GalleryMVC_With_Auth/Controllers/RolesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GalleryMVC_With_Auth.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GalleryMVC_With_Auth.Controllers
+{
+    public class RolesController : Controller
+    {
+
+        ApplicationDbContext context;
+
+        public RolesController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Roles
+        /// <summary>
+        /// Get All Roles
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+
+        /// <summary>
+        /// Create  a New role
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        /// <summary>
+        /// Create a New Role
+        /// </summary>
+        /// <param name="Role"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            context.Roles.Add(Role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
