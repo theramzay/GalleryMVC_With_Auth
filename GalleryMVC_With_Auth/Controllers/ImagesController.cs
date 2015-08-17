48351ede7ffbd816c8ff13078cb38f50191cbd5d
Anton Burkin <ramzzzay@gmail.com>
2015-08-17 17:12:57 +0300

    Refactoring: Akvarel control && view to EF

--- GalleryMVC_With_Auth/Controllers/ImagesController.cs
+++ GalleryMVC_With_Auth/Controllers/ImagesController.cs
@@ -21,7 +21,7 @@ namespace GalleryMVC_With_Auth.Controllers
 
         public ActionResult Akvarel()
         {
-            return View();
+            return View(_repository.Pictures.Where(p => p.Category == "Акварель"));
         }
 
         public ActionResult Guash()
