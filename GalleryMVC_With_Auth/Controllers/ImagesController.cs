127dfa28fe25eedf0266bf16f6e0d3b833fab26d
Anton Burkin <ramzzzay@gmail.com>
2015-08-17 11:26:11 +0300

    Refactoring: ImagesController repository prop

--- GalleryMVC_With_Auth/Controllers/ImagesController.cs
+++ GalleryMVC_With_Auth/Controllers/ImagesController.cs
@@ -1,23 +1,22 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using GalleryMVC_With_Auth.Domain.Abstract;
-using GalleryMVC_With_Auth.Domain.Entities;
 
 namespace GalleryMVC_With_Auth.Controllers
 {
     public class ImagesController : Controller
     {
-        private IPicturesRepository repository;
+        private readonly IPicturesRepository _repository;
 
         public ImagesController(IPicturesRepository repository)
         {
-            this.repository = repository;
+            _repository = repository;
         }
 
         // GET: Images
         public ActionResult Jivopis()
         {
-            return View((object)repository.Pictures.Where(p=>p.Category=="Живопись"));
+            return View(_repository.Pictures.Where(p => p.Category == "Живопись"));
         }
 
         public ActionResult Akvarel()
@@ -39,6 +38,7 @@ namespace GalleryMVC_With_Auth.Controllers
         {
             return View();
         }
+
         public ActionResult Pastel()
         {
             return View();
