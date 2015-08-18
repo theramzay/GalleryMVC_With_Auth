9d4271e1cf0084a30bab56ed28a4b0163c56d0ac
Anton Burkin <ramzzzay@gmail.com>
2015-08-19 01:13:23 +0300

    Refactoring: uploading realization on server side

--- GalleryMVC_With_Auth/Controllers/AdminPanelController.cs
+++ GalleryMVC_With_Auth/Controllers/AdminPanelController.cs
@@ -1,18 +1,22 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
 using GalleryMVC_With_Auth.Domain.Abstract;
 using GalleryMVC_With_Auth.Domain.Entities;
+using GalleryMVC_With_Auth.Helpers;
 
 namespace GalleryMVC_With_Auth.Controllers
 {
     public class AdminPanelController : Controller
     {
-
         private readonly IPicturesRepository _repository;
 
         public AdminPanelController(IPicturesRepository repository)
         {
-            this._repository = repository;
+            _repository = repository;
         }
 
         // GET: AdminPanel
@@ -27,20 +31,37 @@ namespace GalleryMVC_With_Auth.Controllers
         }
 
         [HttpPost]
-        public ActionResult Upload(string path, string tmbpath, string name, string desc,string tag, string cat, decimal prc)
+        public ActionResult Upload(string name, string desc, string tag, string cat, decimal prc, string album,
+            HttpPostedFileBase file)
         {
-            Picture p = new Picture()
+            if (file != null)
             {
-                Path = path,
-                TmbPath = tmbpath,
-                Name = name,
-                Description = desc,
-                Tag = tag,
-                Category = cat,
-                Price = prc
-            };
-            _repository.context.Pictures.Add(p);
-            _repository.context.SaveChanges();
+                var pic = Path.GetFileName(file.FileName);
+                var path = Path.Combine(
+                    Server.MapPath($"~/Content/images/{album}/"), pic);
+                var tmbpath = Path.Combine(
+                    Server.MapPath($"~/Content/images/{album}/tmb"), pic);
+                // file is uploaded
+                file.SaveAs(path);
+
+                ThumbnailCreater.ToTmb(path, tmbpath);
+
+                //Upload info to DB
+                var p = new Picture
+                {
+                    Path = $"/Content/images/{album}/{pic}",
+                    TmbPath = $"/Content/images/{album}/tmb/{pic}",
+                    Name = name,
+                    Description = desc,
+                    Tag = tag,
+                    Category = album,
+                    Price = prc
+                };
+                _repository.context.Pictures.Add(p);
+                _repository.context.SaveChanges();
+            }
+
+
             return View();
         }
     }
