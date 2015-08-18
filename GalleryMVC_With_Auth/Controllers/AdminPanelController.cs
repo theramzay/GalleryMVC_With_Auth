47c0b87b73ee4a24126e03f49b918034563ad91f
Anton Burkin <ramzzzay@gmail.com>
2015-08-18 21:49:14 +0300

    Refactoring: AdminPanel to enable uploading

--- GalleryMVC_With_Auth/Controllers/AdminPanelController.cs
+++ GalleryMVC_With_Auth/Controllers/AdminPanelController.cs
@@ -1,9 +1,20 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using GalleryMVC_With_Auth.Domain.Abstract;
+using GalleryMVC_With_Auth.Domain.Entities;
 
 namespace GalleryMVC_With_Auth.Controllers
 {
     public class AdminPanelController : Controller
     {
+
+        private readonly IPicturesRepository _repository;
+
+        public AdminPanelController(IPicturesRepository repository)
+        {
+            this._repository = repository;
+        }
+
         // GET: AdminPanel
         public ActionResult Index()
         {
@@ -12,6 +23,24 @@ namespace GalleryMVC_With_Auth.Controllers
 
         public ActionResult Upload()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(string path, string tmbpath, string name, string desc,string tag, string cat, decimal prc)
+        {
+            Picture p = new Picture()
+            {
+                Path = path,
+                TmbPath = tmbpath,
+                Name = name,
+                Description = desc,
+                Tag = tag,
+                Category = cat,
+                Price = prc
+            };
+            _repository.context.Pictures.Add(p);
+            _repository.context.SaveChanges();
             return View();
         }
     }
