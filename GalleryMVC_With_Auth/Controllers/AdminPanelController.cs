574c35e99238b21e9385a896faf1bad608360427
Anton Burkin <ramzzzay@gmail.com>
2015-08-18 17:06:13 +0300

    Refactoring: mass translate

---
+++ GalleryMVC_With_Auth/Controllers/AdminPanelController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace GalleryMVC_With_Auth.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        // GET: AdminPanel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+    }
+}
