6171c1fc5019246482179fa426f9531ef9e409bf
Anton Burkin <ramzzzay@gmail.com>
2015-08-28 19:20:29 +0300

    Update (optimization)

--- GalleryMVC_With_Auth/App_Start/BundleConfig.cs
+++ GalleryMVC_With_Auth/App_Start/BundleConfig.cs
@@ -8,9 +8,9 @@ namespace GalleryMVC_With_Auth
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.min.js"));
+                "~/Scripts/jquery-2.1.4.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate.js"));
+                "~/Scripts/jquery.validate.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -18,8 +18,8 @@ namespace GalleryMVC_With_Auth
                 "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/respond.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/magic").Include(
                 "~/Scripts/transp_menu_magic.js"
@@ -28,12 +28,12 @@ namespace GalleryMVC_With_Auth
             bundles.Add(new ScriptBundle("~/bundles/galmagic").Include(
                 "~/Content/blueimp-gallery2/js/blueimp-gallery.min.js",
                 "~/Scripts/BlueimpGal.js",
-                "~/Scripts/jquery.lazyload.js"));
+                "~/Scripts/jquery.lazyload.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/styles").Include(
                 "~/Content/css/hover_effects/component.css",
                 "~/Content/css/hover_effects/normalize.css",
-                "~/Content/css/bootstrap.css",
+                "~/Content/css/bootstrap.min.css",
                 "~/Content/css/search.css",
                 "~/Content/css/site.css",
                 "~/Content/css/transp_menu.css",
