b32974e3f604420217790843fe61973976e91688
Anton Burkin <ramzzzay@gmail.com>
2015-08-28 16:34:16 +0300

    Refactoring: jQuery bundle

--- GalleryMVC_With_Auth/App_Start/BundleConfig.cs
+++ GalleryMVC_With_Auth/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace GalleryMVC_With_Auth
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate.js"));
 
