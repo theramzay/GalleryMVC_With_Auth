2b07e2b157f0ab4714fb6a8c4a01d7865103024d
Anton Burkin <ramzzzay@gmail.com>
2015-10-07 22:57:14 +0300

    Update: global reinstall packages

--- GalleryMVC_With_Auth/App_Start/NinjectWebCommon.cs
+++ GalleryMVC_With_Auth/App_Start/NinjectWebCommon.cs
@@ -1,40 +1,40 @@
-using System;
-using System.Web;
-using GalleryMVC_With_Auth.App_Start;
-using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-using Ninject;
-using Ninject.Web.Common;
-using WebActivatorEx;
-
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof (NinjectWebCommon), "Start")]
-[assembly: ApplicationShutdownMethod(typeof (NinjectWebCommon), "Stop")]
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(GalleryMVC_With_Auth.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(GalleryMVC_With_Auth.App_Start.NinjectWebCommon), "Stop")]
 
 namespace GalleryMVC_With_Auth.App_Start
 {
-    public static class NinjectWebCommon
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+
+    public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
-        ///     Starts the application
+        /// Starts the application
         /// </summary>
-        public static void Start()
+        public static void Start() 
         {
-            DynamicModuleUtility.RegisterModule(typeof (OnePerRequestHttpModule));
-            DynamicModuleUtility.RegisterModule(typeof (NinjectHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-
+        
         /// <summary>
-        ///     Stops the application.
+        /// Stops the application.
         /// </summary>
         public static void Stop()
         {
             bootstrapper.ShutDown();
         }
-
+        
         /// <summary>
-        ///     Creates the kernel that will manage your application.
+        /// Creates the kernel that will manage your application.
         /// </summary>
         /// <returns>The created kernel.</returns>
         private static IKernel CreateKernel()
@@ -56,11 +56,11 @@ namespace GalleryMVC_With_Auth.App_Start
         }
 
         /// <summary>
-        ///     Load your modules or register your services here!
+        /// Load your modules or register your services here!
         /// </summary>
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-        }
+        }        
     }
 }
