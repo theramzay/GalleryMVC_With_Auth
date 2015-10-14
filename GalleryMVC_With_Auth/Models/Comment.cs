892aeb45de2034b99bb45337fa177489f9f0d2dc
Anton Burkin <ramzzzay@gmail.com>
2015-10-15 00:27:43 +0300

    Add: view model to comments

---
+++ GalleryMVC_With_Auth/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GalleryMVC_With_Auth.Models
+{
+    public class CommentModel
+    {
+        public string Text { get; set; }
+    }
+}
