ae3cafe8d929a7a03e13c635b3f4889ab3b43cc5
Anton Burkin <ramzzzay@gmail.com>
2015-10-19 22:27:56 +0300

    Update: project

---
+++ GalleryMVC_With_Auth.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GalleryMVC_With_Auth.Controllers.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IndexAlbumShowTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
