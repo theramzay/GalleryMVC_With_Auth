c5e593937885114a0d1bbc5ea31991b165e48ea4
Anton Burkin <ramzzzay@gmail.com>
2015-10-23 18:05:38 +0300

    Update: project

--- GalleryMVC_With_Auth.Tests/UnitTest1.cs
+++ GalleryMVC_With_Auth.Tests/UnitTest1.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using GalleryMVC_With_Auth.Controllers;
+using GalleryMVC_With_Auth.Domain.Abstract;
+using GalleryMVC_With_Auth.Domain.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace GalleryMVC_With_Auth.Tests
 {
@@ -9,6 +16,19 @@ namespace GalleryMVC_With_Auth.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            //Arrange
+            Mock<IQueryable<Album>> albumMock = new Mock<IQueryable<Album>>();
+            Mock<IPicturesRepository> repMock = new Mock<IPicturesRepository>();
+
+            repMock.Setup(x => x.Albums.Select(xy => xy).ToList()).Returns(new List<Album> {new Album {Description = "desc"} });
+            List<Album> f = new List<Album> {new Album {Description = "desc"}};
+
+            //Act
+            HomeController hc = new HomeController(repMock.Object);
+
+            //Assert
+            var c = hc.Index();
+
         }
     }
 }
