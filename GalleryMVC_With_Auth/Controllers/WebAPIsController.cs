6b4f11359124b7f1e2887b05b28497352e1aaee9
Anton Burkin <ramzzzay@gmail.com>
2015-11-18 01:15:36 +0300

    Refactoring: api controller

---
+++ GalleryMVC_With_Auth/Controllers/WebAPIsController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using GalleryMVC_With_Auth.Domain.Abstract;
+using GalleryMVC_With_Auth.Models;
+
+namespace GalleryMVC_With_Auth.Controllers
+{
+    public class WebAPIsController : ApiController
+    {
+        private readonly IPicturesRepository _repository;
+
+        public WebAPIsController(IPicturesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<AlbumModel> GetAlbums()
+        {
+            return
+                _repository.Albums.Select(
+                    a =>
+                        new AlbumModel
+                        {
+                            Id = a.Id,
+                            Description = a.Description,
+                            ImgPath = a.ImgPath,
+                            Link = a.Link,
+                            Name = a.Name
+                        }).ToList();
+        }
+
+        public string Get()
+        {
+            return "U in web api controller!";
+        }
+
+        public List<PictureModel> GetFound(string search)
+        {
+            return search[0] == '#'
+                ? _repository.Pictures.Where(p => p.Tag.Name.Contains(search))
+                    .Select(
+                        p =>
+                            new PictureModel
+                            {
+                                Id = p.Id,
+                                Name = p.Name,
+                                Description = p.Description,
+                                Price = p.Price,
+                                Path = p.Path,
+                                TmbPath = p.TmbPath
+                            })
+                    .ToList()
+                : _repository.Pictures.Where(p => p.Name.Contains(search))
+                    .Select(
+                        p =>
+                            new PictureModel
+                            {
+                                Id = p.Id,
+                                Name = p.Name,
+                                Description = p.Description,
+                                Price = p.Price,
+                                Path = p.Path,
+                                TmbPath = p.TmbPath
+                            })
+                    .ToList();
+        }
+    }
+}
