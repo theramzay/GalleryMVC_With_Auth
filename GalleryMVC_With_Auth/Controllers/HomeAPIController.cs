ba21f82e11759ff0560a4f757abc1a54e26f2264
Anton Burkin <ramzzzay@gmail.com>
2015-11-17 21:01:28 +0300

    Add: Web Api Home Controller

---
+++ GalleryMVC_With_Auth/Controllers/HomeAPIController.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Http;
+using GalleryMVC_With_Auth.Domain.Abstract;
+
+namespace GalleryMVC_With_Auth.Controllers
+{
+    public class HomeAPIController : ApiController
+    {
+
+    }
+}
--- GalleryMVC_With_Auth/Global.asax.cs
+++ GalleryMVC_With_Auth/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -10,6 +11,7 @@ namespace GalleryMVC_With_Auth
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
