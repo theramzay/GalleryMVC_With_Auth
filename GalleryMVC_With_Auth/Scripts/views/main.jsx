01b8ac31485ab16cabed786971c16898c193dda8
Anton Burkin <ramzzzay@gmail.com>
2015-12-01 14:36:06 +0300

    Add: softy scroll effect

--- GalleryMVC_With_Auth/Scripts/views/main.jsx
+++ GalleryMVC_With_Auth/Scripts/views/main.jsx
@@ -1,48 +1,7 @@
 ﻿var Albums = React.createClass({
-    loadAlbumsFromServer: function () {
-        var xhr = new XMLHttpRequest();
-        xhr.open('get', this.props.url, true);
-        xhr.onload = function () {
-            var data = JSON.parse(xhr.responseText);
-            this.setState({ data: data });
-        }.bind(this);
-        xhr.send();
-    },
-    getInitialState: function () {
-        return { data: [] };
-    },
-    componentDidMount: function () {
-        this.loadAlbumsFromServer();
-        window.setInterval(this.loadAlbumsFromServer, this.props.pollInterval);
-    },
-    //componentWillReceiveProps: function () {  //TODO: create one get
-    //    this.loadFromServer();
-    //},
-    render: function () {
-        return (
-            <div>
-                    {this.state.data.map(
-                    function(a) {
-                    return (
-                        <figure className="effect-lily">
-            <img src={a.ImgPath} alt="img01" />
-            <figcaption>
-                <h2><span>{a.Name}</span></h2>
-                <p>{a.Description}</p>
-                <a href="#pictures" onClick={ImageSwitch.bind(this,a.Id)}>Открыть</a>
-            </figcaption>
-                        </figure>
-                    );
-                    })}
-                </div>
-            );
-    }
-});
-
-var Pictures = React.createClass({
     loadFromServer: function () {
         var xhr = new XMLHttpRequest();
-        xhr.open("get", this.props.url, true);
+        xhr.open('get', this.props.url, true);
         xhr.onload = function () {
             var data = JSON.parse(xhr.responseText);
             this.setState({ data: data });
@@ -55,14 +14,59 @@ var Pictures = React.createClass({
     componentWillReceiveProps: function () {
         this.loadFromServer();
     },
+    componentWillMount: function () {
+        this.loadFromServer();
+    },
+    //componentWillReceiveProps: function () {  //TODO: create one get
+    //    this.loadFromServer();
+    //},
     render: function () {
         return (
-            <div>
+            <div className="grid">
+                    {this.state.data.map(
+                    function(a) {
+                    return (
+                        <figure className="effect-lily">
+            <img src={a.ImgPath} alt="img01" />
+            <figcaption>
+                <h2><span>{a.Name}</span></h2>
+                <p>{a.Description}</p>
+                <a href="#images" className="smoothScroll" onClick={ImageSwitch.bind(this,a.Id)}>Открыть</a>
+            </figcaption>
+                        </figure>
+                    );
+                    })}
+                </div>
+            );
+    }
+});
+
+var Pictures = React.createClass({
+    loadFromServer: function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open("get", this.props.url + this.props.id, true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        }.bind(this);
+        xhr.send();
+    },
+    getInitialState: function () {
+        return { data: [] };
+    },
+    componentWillReceiveProps: function () {
+        this.loadFromServer();
+    },
+    componentWillMount: function () {
+        this.loadFromServer();
+    },
+    render: function () {
+        return (
     <div id="links">
         {this.state.data.map(function(p) {
             return (
             <figure className="effect-milo">
-                <img className="lazy" data-original={p.TmbPath} alt={p.Description} />
+                <img className="lazy" src={p.TmbPath} data-original={p.TmbPath} alt={p.Description} />
                 <figcaption>
                     <h2>
                         <span>{p.Description}</span>
@@ -73,17 +77,19 @@ var Pictures = React.createClass({
                 </figcaption>
             </figure>);
         })}
+
     </div>
-            </div>
         );
     }
 });
 
 
-var ImageSwitch = function (id, o) {
-    var url = "/api/WebApis/GetPictures/?id=" + id;
-    React.render(<Pictures url={url} />, document.getElementById('pictures'));
+function ImageSwitch(id) {
+    console.log("Debug log, ID is: " + id);
+    $.getScript("/bundles/galmagic?v=i3kUWqQQpdXyuxIE00qkjs5ynWxapRWXXmicTk_JDKE1");
+    
+    React.render(<Pictures url="/api/WebApis/GetPictures/?id=" id={id}  />, document.getElementById('images'));
 }
-
+//React.render(<Pictures url="/api/WebApis/GetPictures/?id=" id={3}  />, document.getElementById('images'));
 
 React.render(<Albums url="/api/WebApis/GetAlbums" pollInterval={200000} />, document.getElementById('albums'));
